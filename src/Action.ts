f21986cb7ca0a7a32e4bd3aff6f0678db1cf211f
buhi <yinshang1992@gmail.com>
2017-04-20 11:35:59 +0800

    remove cache feature for custom action

--- src/Action.ts
+++ src/Action.ts
@@ -12,15 +12,8 @@ export interface ActionDefinition<T>{
     getSearch?(data:T):{[key:string]:string},
     getSearch?(data:T[]):{[key:string]:string},
     getBody?(data:T):any,
-    getBody?(data:T[]):any,
-    cacheTime?: number //seconds
+    getBody?(data:T[]):any
 }
-
-type ActionCache = {
-    [action_url:string]:{
-        LastCachedTime:number,
-        cachedPromise:Promise<any>}
-};
 
 export interface ActionOption<T>{
     baseUrl:string,
@@ -32,8 +25,6 @@ export interface ActionOption<T>{
 
 export function RestfulActionFactory<T>(option:ActionOption<T>){
     const {actionDef,getDataFromResponse,getID,baseUrl} = option;
-    let ActionCacheMap = {} as ActionCache;
-    let isRequesting;
     return function RestfulAction(data?,requestInit?:RequestInit) {
         const nextRequestInit:RequestInit = {...requestInit};
         let url = baseUrl +"/" + actionDef.path.replace(/(:\w+)(?=\/|$)/g,function(match){
@@ -45,24 +36,8 @@ export function RestfulActionFactory<T>(option:ActionOption<T>){
             nextRequestInit.body = JSON.stringify(actionDef.getBody(data));
         if(actionDef.getSearch)
             url += buildQuery(actionDef.getSearch(data));
-        if(actionDef.cacheTime) {
-            const cached = ActionCacheMap[url];
-            if(cached) {
-                const {LastCachedTime, cachedPromise} = cached;
-                if (Date.now() - LastCachedTime < actionDef.cacheTime)
-                    isRequesting = cachedPromise;
-            }
-        }
-        if(!isRequesting) {
-            isRequesting = option.fetch(url, nextRequestInit).then(res => res.json()).then(res => {
-                return getDataFromResponse(res, actionDef.key);
-            });
-            if (actionDef.cacheTime)
-                ActionCacheMap[url] = {
-                    cachedPromise: isRequesting,
-                    LastCachedTime: Date.now()
-                };
-        }
-        return isRequesting;
+        return option.fetch(url, nextRequestInit).then(res => res.json()).then(res => {
+            return getDataFromResponse(res, actionDef.key);
+        });
     };
 }
