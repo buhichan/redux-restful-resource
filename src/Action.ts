7ac325178e8ef6a4c9cd10fb30249698322e04c8
buhi <yinshang1992@gmail.com>
2017-04-20 10:47:37 +0800

    修复action method没有生效的bug

--- src/Action.ts
+++ src/Action.ts
@@ -39,6 +39,8 @@ export function RestfulActionFactory<T>(option:ActionOption<T>){
         let url = baseUrl +"/" + actionDef.path.replace(/(:\w+)(?=\/|$)/g,function(match){
                 return data[match.slice(1)] || ""
             });
+        if(actionDef.method)
+            nextRequestInit.method = actionDef.method.toUpperCase();
         if(actionDef.getBody && data)
             nextRequestInit.body = JSON.stringify(actionDef.getBody(data));
         if(actionDef.getSearch)
